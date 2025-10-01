e2194049ae8a48f66811c48b985a8c1b3dd30b33
ehang0404 <ehang.workspace@gmail.com>
2025-10-01 21:21:20 +0000

    feat: add multisig wallet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,7 @@
 import "hardhat-storage-layout"
 import "hardhat-storage-layout-changes";
 import "./tasks/deploy-final";
+import "./tasks/multisig";
 
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
